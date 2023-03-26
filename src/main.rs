553ab9f5a17b3dabf69ace30a7acb83d00522eb3
Yahli Mariansky <yahlimem@gmail.com>
2023-03-26 20:09:10 +0300

    nannou default!

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use nannou::prelude::*;
+
+
+struct Model {
+    _window: window::Id,
+}
+
+fn main() {
+    nannou::app(model).update(update).run();
+}
+
+fn model(app: &App) -> Model {
+    let _window = app.new_window().view(view).build().unwrap();
+    Model {
+        _window 
+    }
+}
+
+fn update(app: &App, model: &mut Model, _update: Update) {
+
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+
+
+    draw.background().color(WHITE);
+
+    draw.to_frame(app, &frame).unwrap();
+
+}
